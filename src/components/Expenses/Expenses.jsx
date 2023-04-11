8e02e0df44f05789058cbc42a810529e9bcd2757
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-04-11 17:58:43 +0900

    조건부 렌더링

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -11,19 +11,29 @@ export const Expenses = (props) => {
     setFilteredYear(selectedYear);
   }
 
+  const filteredExpense = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if (filteredExpense.length > 0) {
+    expensesContent = filteredExpense.length > 0 && filteredExpense.map(expense => {
+      return (
+        <ExpenseItem
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+          key={expense.id}
+        />
+      );
+    })
+  }
+
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map(expense => {
-        return (
-          <ExpenseItem
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount}
-            key={expense.id}
-          />
-        );
-      })}
+      {expensesContent}
     </Card>
   );
 };
